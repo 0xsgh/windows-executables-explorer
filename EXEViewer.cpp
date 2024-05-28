b869348295e1878e7e261787a018e848845321b0
0xsgh <0xsgh@github.com>
2024-05-28 23:22:16 +0200

    [15] Some clean-up
    - Removed the "Raw Data" tab since I haven't found a use for it (and don't
      expect to). The underlying raw data is still available in case there is
      other information I need to extract and display from it.
    
    - Made it possible to switch between multiple open files (it was already
      possible to *load* multiple files). We can now view information for a
      file by double-clicking on it in the file list (the double-click is
      intended to clear any multi-selection).
    
    - Made it possible to unload loaded files (by right-clicking on them in
      the file list and selecting "Unload selection" in the context menu).
      The sole reason the file list widget has multi-selection enabled is to
      allow multiple files to be unloaded at once.

--- EXEViewer.cpp
+++ EXEViewer.cpp
@@ -35,38 +35,10 @@ EXEViewer::EXEViewer( EXEFile&& loadedEXEFile,
 : QTabWidget( parentWidget )
 , m_loadedEXEFile( std::move( loadedEXEFile ) )
 {
-    setUpRawDataTab();
     setUpFileHeadersTab();
     setUpSectionHeadersTab();
     setUpImportsTab();
     setUpExportsTab();
-}
-
-void
-EXEViewer::setUpRawDataTab()
-{
-    auto rawDataViewer = new QPlainTextEdit;
-    addTab( rawDataViewer, "Raw Data" );
-
-    rawDataViewer->setReadOnly( true );
-
-    auto rawDataAsText = QString();
-    for ( auto i = 0; i < m_loadedEXEFile.rawBytes.size(); i++ )
-    {
-        rawDataAsText += QString( " %1" ).arg( m_loadedEXEFile.rawBytes[ i ],
-                                               2, 16, QChar( '0' ) ).toUpper();
-
-        if ( ( i + 1 ) % 24 == 0 )
-        {
-            rawDataViewer->appendPlainText( rawDataAsText );
-            rawDataAsText.clear();
-        }
-
-        if ( ( i % 1024 ) == 0 )
-        {
-            QApplication::processEvents();
-        }
-    }
 }
 
 void
